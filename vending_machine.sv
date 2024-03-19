33157f011ce161cae2b012489a49b02ca8afcadb
Stork1323 <tamd132003@gmail.com>
2024-03-19 18:36:58 +0700

    updated

--- vending_machine.sv
+++ vending_machine.sv
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ps
+
 module vending_machine(
 	input logic nickle_i,
 	input logic dime_i,
@@ -17,70 +19,72 @@ module vending_machine(
 		S3: 15$
 	*/
 	
-	state_e state = S0;
+	state_e current_state, next_state = S0;
 	//state_e next_state = S0;
 	
-	always_ff @(posedge clk_i) begin : update_next_state
+	always @(posedge clk_i) begin : update_next_state
 	
-//		if (nickle_i) coins <= 3'b001;
-//		if (dime_i) coins <= 3'b010;
-//		if (quarter_i) coins <= 3'b100;
-		//else coins <= 3'b000;
+//		if (nickle_i) coins = 3'b001;
+//		if (dime_i) coins = 3'b010;
+//		if (quarter_i) coins = 3'b100;
+		//else coins = 3'b000;
 		
-		unique case (state)
+		current_state = next_state;
+		
+		unique case (current_state)
 			S0: begin
-				soda_o <= 1'b0;
-				change_o <= 3'b000;
-				if (nickle_i) state <= S1;
-				else if (dime_i) state <= S2;
+				soda_o = 1'b0;
+				change_o = 3'b000;
+				if (nickle_i) next_state = S1;
+				else if (dime_i) next_state = S2;
 				else if (quarter_i) begin
-					soda_o <= 1'b1;
-					change_o <= 3'b001;
-					state <= S0;
+					soda_o = 1'b1;
+					change_o = 3'b001;
+					next_state = S0;
 				end
-				else state <= state;
+				else next_state = current_state;
 			 end
 			S1: begin
-				if (nickle_i) state <= S2;
-				else if (dime_i) state <= S3;
+				if (nickle_i) next_state = S2;
+				else if (dime_i) next_state = S3;
 				else if (quarter_i) begin
-					soda_o <= 1'b1;
-					change_o <= 3'b010;
-					state <= S0;
+					soda_o = 1'b1;
+					change_o = 3'b010;
+					next_state = S0;
 				end
-				else state <= state;
+				else next_state = current_state;
 			 end
 			S2: begin
-				if (nickle_i) state <= S3;
+				if (nickle_i) next_state = S3;
 				else if (dime_i) begin
-					soda_o <= 1'b1;
-					change_o <= 3'b000;
-					state <= S0;
+					soda_o = 1'b1;
+					change_o = 3'b000;
+					next_state = S0;
 				end
 				else if (quarter_i) begin
-					soda_o <= 1'b1;
-					change_o <= 3'b011;
-					state <= S0;
+					soda_o = 1'b1;
+					change_o = 3'b011;
+					next_state = S0;
 				end
-				else state <= state;
+				else next_state = current_state;
 			 end
 			S3: begin
 				if (nickle_i) begin
-					soda_o <= 1'b1;
-					change_o <= 3'b000;
-					state <= S0;
+					soda_o = 1'b1;
+					change_o = 3'b000;
+					next_state = S0;
 				end
 				else if (dime_i) begin
-					soda_o <= 1'b1;
-					change_o <= 3'b001;
-					state <= S0;
+					soda_o = 1'b1;
+					change_o = 3'b001;
+					next_state = S0;
 				end
 				else if (quarter_i) begin
-					soda_o <= 1'b1;
-					change_o <= 3'b100;
-					state <= S0;
+					soda_o = 1'b1;
+					change_o = 3'b100;
+					next_state = S0;
 				end
-				else state <= state;
+				else next_state = current_state;
 			 end
 		endcase
 	end
